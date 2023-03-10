3badf0cdbcceacacbe23c1266292c739239676f8
DANIEL SUAREZ <danielsuarez@DANIELs-MBP.lan>
2023-03-10 10:15:29 -0500

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto px-5">
+      <h1 className="font-ls text-3xl text-amber-600 mb-2">About</h1>
+      <p className="mb-2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac
+        iaculis odio, a faucibus ex. Vestibulum odio purus, tempor sed mattis
+        vitae, fermentum eu sem. In viverra purus vel dui varius efficitur. Nunc
+        velit nulla, convallis quis ligula dictum, tempor feugiat lacus. Nam
+        lacus tortor, interdum sit amet convallis sed, accumsan egestas magna.
+        Vestibulum volutpat maximus lacus, et ullamcorper leo hendrerit ac.
+        Integer semper egestas posuere. Morbi vestibulum lacus neque, sit amet
+        tincidunt nulla commodo vitae.
+      </p>
+      <p className="mb-2">
+        Suspendisse at justo et metus convallis euismod. Sed facilisis a nulla
+        nec pretium. Proin condimentum sapien pellentesque, facilisis ipsum sit
+        amet, rhoncus felis. Pellentesque habitant morbi tristique senectus et
+        netus et malesuada fames ac turpis egestas. Ut laoreet malesuada dui non
+        venenatis. Curabitur malesuada erat ac felis condimentum dignissim.
+        Morbi maximus, est sit amet aliquet commodo, leo ex blandit leo, ut
+        iaculis enim purus eu eros.
+      </p>
+      <p className="mb-2">
+        Phasellus tortor massa, semper sit amet nunc eleifend, eleifend
+        condimentum nisi. Vivamus nulla libero, bibendum a pellentesque a,
+        pellentesque ut nisi. Etiam consequat ac ipsum ut laoreet. Quisque ut
+        velit pulvinar, sagittis nulla nec, mollis sem. Integer vestibulum nunc
+        vitae tempor convallis. Integer at velit ullamcorper, accumsan lacus ut,
+        aliquam ipsum. Praesent tincidunt turpis non nulla condimentum, sed
+        condimentum tortor blandit. Fusce ac congue nulla, in rutrum arcu. Nulla
+        facilisi. Ut sodales iaculis ligula, eu maximus velit condimentum nec.
+        Donec libero sem, aliquet nec ligula sed, consequat lacinia nunc. Etiam
+        interdum magna mollis, rhoncus eros quis, placerat elit. Proin ac ligula
+        vestibulum, rhoncus ante nec, blandit ex. Vivamus consequat dui sed
+        libero volutpat, et varius magna tristique. Nulla sed mollis sem, et
+        scelerisque velit.
+      </p>
+    </div>
+  );
+}
