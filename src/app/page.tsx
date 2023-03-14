fcd853880bb728008690c1b3866729381bffa81c
DANIEL SUAREZ <danielsuarez@DANIELs-MBP.lan>
2023-03-14 12:56:05 -0400

    added fetch results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Results from "./components/Results";
 
-const inter = Inter({ subsets: ["latin"] });
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
-  return <h1 className="text-red-400">home</h1>;
+type GENRE = "fetchTrending" | "fetchTopRated";
+
+interface HomeProps {
+  searchParams: SearchParams;
+}
+interface SearchParams {
+  genre: GENRE;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/movie/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("error validationg");
+  }
+
+  const data = await res.json();
+
+  return (
+    <h1 className="text-red-400">
+      <Results results={data.results} />
+    </h1>
+  );
 }
