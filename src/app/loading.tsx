bfb5c473a8a48abe0f5e1940dc33aba1d4d3d724
DANIEL SUAREZ <danielsuarez@DANIELs-MBP.lan>
2023-03-16 09:55:20 -0400

    add loader screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-20">
+      <img src="loader.svg" alt="loading..." />
+    </div>
+  );
+}
